1449103172c8e821052b49565df841936f3be51f
skylerdufour40 <andrea.machniak@myhpsd.org>
2026-09-23 18:10:41 +0000

    Pending changes exported from your codespace

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'app-information-v1';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
